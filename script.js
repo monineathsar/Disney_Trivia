3fc7fe08f2aa0b9a08307abb038bbea9ef5836ea
Monineath Sar <monineathsar@gmail.com>
2022-06-13 21:41:01 -0700

    working start button and trivia API

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-// let questionEl = document.querySelector(".questionText");
+let questionEl = document.querySelector(".questionText");
+// main page elements
+const mainPageSection = document.getElementById("mainPage");
+
+const gamePageSection = document.getElementById("startGamePage");
+
+// Highscore page elements
+const highscoreSection = document.getElementById("highscorePage");
+
+const mainPageBtn = document.getElementById("mainPageBtn");
+mainPageBtn.setAttribute("onclick", "viewMainPage()");
+
+// highscore FORM page elements
+const hsFormSection = document.getElementById("formPage");
+
+const saveFormBtn = document.getElementById("saveBtn");
+saveFormBtn.setAttribute("onclick", "viewMainPage()");
+
+const homeContainer = document.querySelector(".container");
+const startgameBtn = homeContainer.querySelector("#startBtn");
+const viewHsBtn = document.getElementById("highscoresBtn");
+viewHsBtn.setAttribute("onclick", "viewHighScore()");
+
+const giphyKey = "G0bL1VhIULA0XwlHEwdsZFsRp2kbEO4k";
+
+let questionsArray;
+let currentQuestion;
+
 // // // Variables defined 
 // // var game = $("#game");
 // // var questEl = $('#question');
@@ -11,35 +38,91 @@
 // // var startButton = $("#startbutton");
 
 
-// function getMarvel() {
-//     return new Promise(function(resolve, reject){
-//     fetch('https://the-trivia-api.com/api/questions?limit=10&difficulty=medium&tags=marvel').then(function(response){
-//         response.json().then(function(data){
-//           resolve(data)
-//         });
-//       }).catch(function(error){
-//         reject(error);
-//       })
-//     });
-//   }
+async function getMarvel() {
+    return new Promise(function(resolve, reject){
+    fetch('https://the-trivia-api.com/api/questions?limit=10&difficulty=easy&tags=marvel').then(function(response){
+        response.json().then(function(data){
+          resolve(data)
+        });
+      }).catch(function(error){
+        reject(error);
+      })
+    });
+}
 
-// getMarvel().then(function(result){
-//   console.log(result)
-// });
+async function getGiphy() {
+  let giphyUrl = 'https://api.giphy.com/v1/gifs/G0bL1VhIULA0XwlHEwdsZFsRp2kbEO4k?api_key=' + giphyKey;
+  let giphyPng = await fetch(giphyUrl)
+    .then(function(response){
+    response.json().then(function(data){
+      resolve(data)
+    });
+  return giphyPng;
+  // }).catch(function(error){
+  //   reject(error);
+  });
+};
 
-// function getTrivia() {
-//   return new Promise(function(resolve, reject){
-//   fetch('https://opentdb.com/api.php?amount=50&category=11&type=multiple').then(function(response){
-//     response.json().then(function(data){
-//       resolve(data)
-//     });
-//   }).catch(function(error){
-//     reject(error);
-//   })
-// });
-// }
+// to fetch trivia API
+function generateQuestions() {
+  viewGamePage();
+  if (currentQuestion > 9) {
+    score = correctAnswersCount;
+    clearInterval(timer);
+    gameOver();
+    viewFormPage();
+    return;
+  }
 
-// getTrivia().then(function(data){
+  let questionText = document.querySelector(".questionText");
+  let answerList = document.querySelector(".answerList");
+
+  let choices = questionsArray[currentQuestion].incorrectAnswers;
+  choices.push(questionsArray[currentQuestion].correctAnswer);
+  
+  let randomChoices = _.sample(choices, 4);
+  console.log(randomChoices);
+
+  questionText.innerHTML = '<div class="questionText"><span>' + (currentQuestion+1) + '. ' + questionsArray[currentQuestion].question + '</span>'; 
+  //shows answers choices in quiz box in order it was written in questions object array
+  answerList.innerHTML = '<div class="option"><span>'+ randomChoices[0] +'</span></div>'
+  + '<div class="option"><span>'+ randomChoices[1] +'</span></div>'
+  + '<div class="option"><span>'+ randomChoices[2] +'</span></div>'
+  + '<div class="option"><span>'+ randomChoices[3] +'</span></div>'; 
+
+  //grabs all choices array from questions array object
+  let options = answerList.querySelectorAll(".option");
+
+  //adds even listener for when player clicks on their choice
+  for (i=0; i < options.length; i++){
+    options[i].setAttribute("onclick", "optionSelected(this)");
+  }
+}
+
+function optionSelected(answer) {
+  if(answer.innerText === questionsArray[currentQuestion].correctAnswer) {
+    console.log("yay");
+  }
+
+  currentQuestion++;
+  generateQuestions();
+}
+
+// start game funtion
+startgameBtn.onclick = async function() {
+  document.querySelector(".timeText").innerHTML = "Time: ";
+  document.getElementById("secsLeft").style.display = "block";
+
+  timeLeft = 15;
+  currentQuestion = 0;
+  //timer();
+  await getMarvel(questionsArray).then(result => {
+    questionsArray = result;
+    console.log(questionsArray);
+  });
+  generateQuestions(currentQuestion);
+}
+// getGiphy().then(function(data){
 //   for(let i = 0; i < data.results.length; i++){
 //     if(data.results[i].question.includes("Marvel")){
 //       let question = document.createElement("p");
@@ -52,7 +135,7 @@
 // });
 
 
-// // Set clock, remove button, and start 60 seconds, begin game
+// Set clock, remove button, and start 60 seconds, begin game
 // function gameInit() {
 //     highScore = JSON.parse(localStorage.getItem("xxx"));
 //     correctInput = 0;
@@ -65,7 +148,7 @@
 // }
 
 
-// // Function to display questions and correct answers
+// Function to display questions and correct answers
 // function displayQuestion(myObject) {
 
 //     questEl.text(myObject.question);
@@ -98,9 +181,8 @@
 // }
 
 
-// // Create the timer function and to have run smoothly
+// Create the timer function and to have run smoothly
 // function timer() {
-//     game.text(`You have ${timeLeft} seconds left.`);
 //     var timeInterval = setInterval(function () {
 
 //         if (timeLeft > 0 && quizIndex !== quizObject.length){
@@ -114,17 +196,18 @@
 //     },1000);
 // };
 
-// // Write an event listener for start button
-// // Start button will begin game
+// Write an event listener for start button
+// Start button will begin game
 // startButton.on("click", function(event) {
 //     let element = event.target;
 //     if (element.matches("button") === true) {
 //         // Initialize Game
 //         gameInit();
 //     }
+//     console.log(event);
 // })
 
-// // Write an event listener for answer selection
+// Write an event listener for answer selection
 // answerOrdered.on("click", function(event) {
 //     let element = event.target;
 //     if (element.matches("li") === true) {
@@ -139,71 +222,37 @@
 //         };
 //     };
 // });
-
-//             answerOrdered.html('');
-//             displayQuestion(quizObject[quizIndex]);
-//         };
-//     };
-// });
-
 // Monineath's section >>>>>
-// main page elements
-var mainPageSection = document.getElementById("mainPage");
-
-var viewHsBtn = document.getElementById("highscoresBtn");
-viewHsBtn.setAttribute("onclick", "viewHighScore()");
-
-// Highscore page elements
-var highscoreSection = document.getElementById("highscorePage");
-
-var mainPageBtn = document.getElementById("mainPageBtn");
-mainPageBtn.setAttribute("onclick", "viewMainPage()");
-
-// highscore FORM page elements
-var hsFormsection = document.getElementById("formPage");
-
-var saveFormBtn = document.getElementById("saveBtn");
-saveFormBtn.setAttribute("onclick", "viewMainPage()");
 
 // start game page elements
-var startGameSection = 
+// var startGameSection = 
 
 // to display highscore page
 function viewHighScore() {
   highscoreSection.style.display = "block";
   mainPageSection.style.display = "none";
-  hsFormsection.style.display = "none";
+  hsFormSection.style.display = "none";
+}
 
-
-    //pulls stored score arrays from local storage 
-    // var storedScoreArray = JSON.parse(localStorage.getItem("data"));
-    // if (storedScoreArray !== null) { //sorts highscores from highest to lowest 
-    //   storedScoreArray.sort((a, b) => parseFloat(b.score) - parseFloat(a.score));
-    //   scoresarray = storedScoreArray;
-    // }
-
-    //show stored highscores in highscore box
-
-};
 // to display main page
 function viewMainPage() {
   mainPageSection.style.display = "block";
   highscoreSection.style.display = "none";
-  hsFormsection.style.display = "none";
+  hsFormSection.style.display = "none";
 }
+
 // to diplay highscore FORM page
 function viewFormPage() {
   mainPageSection.style.display = "none";
   highscoreSection.style.display = "none";
-  hsFormsection.style.display = "block";
+  hsFormSection.style.display = "block";
 }
+
 // to display game page
-function startGame() {
+function viewGamePage() {
   gamePageSection.style.display = "block";
   mainPageSection.style.display = "none";
   highscoreSection.style.display = "none";
-  hsFormsection.style.display = "none";
+  hsFormSection.style.display = "none";
 }
 // <<<<< Monineath's section
-
-// fdafas
