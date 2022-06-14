637450e3a97cf2ded15d7d7d5310b6a1371c2500
Monineath Sar <monineathsar@gmail.com>
2022-06-14 13:23:40 -0700

    working Trivia API  with corresponding Giphy API

--- script.js
+++ script.js
@@ -1,46 +1,34 @@
-let questionEl = document.querySelector(".questionText");
-// main page elements
+// main page global variables
 const mainPageSection = document.getElementById("mainPage");
-
-const gamePageSection = document.getElementById("startGamePage");
-
-// Highscore page elements
-const highscoreSection = document.getElementById("highscorePage");
-
-const mainPageBtn = document.getElementById("mainPageBtn");
-mainPageBtn.setAttribute("onclick", "viewMainPage()");
-
-// highscore FORM page elements
-const hsFormSection = document.getElementById("formPage");
-
-const saveFormBtn = document.getElementById("saveBtn");
-saveFormBtn.setAttribute("onclick", "viewMainPage()");
-
 const homeContainer = document.querySelector(".container");
 const startgameBtn = homeContainer.querySelector("#startBtn");
 const viewHsBtn = document.getElementById("highscoreBtn");
 viewHsBtn.setAttribute("onclick", "viewHighScore()");
 
-const giphyKey = "G0bL1VhIULA0XwlHEwdsZFsRp2kbEO4k";
-
+// Game page global variables
+const gamePageSection = document.getElementById("startGamePage");
 let questionsArray;
 let currentQuestion;
 
-// // // Variables defined 
-// // var game = $("#game");
-// // var questEl = $('#question');
-// // var answerOrdered = $("#answers");
-// // var answerAlert = $("#answeralert");
-// // var correctInput;
-// // var incorrectInput;
-// // var quizIndex;
-// // var timeLeft;
-// // var startButton = $("#startbutton");
+// Highscore page global variables
+const highscoreSection = document.getElementById("highscorePage");
+const mainPageBtn = document.getElementById("mainPageBtn");
+mainPageBtn.setAttribute("onclick", "viewMainPage()");
+
+// highscore FORM page global variables
+const hsFormSection = document.getElementById("formPage");
+const saveFormBtn = document.getElementById("saveBtn");
+saveFormBtn.setAttribute("onclick", "viewMainPage()");
+
+// Giphy API key
+const giphyKey = "G0bL1VhIULA0XwlHEwdsZFsRp2kbEO4k";
 
 
+// Marvel Trivia API fecth
 async function getMarvel() {
     return new Promise(function(resolve, reject){
-    fetch('https://the-trivia-api.com/api/questions?limit=10&difficulty=easy&tags=marvel').then(function(response){
+    fetch('https://the-trivia-api.com/api/questions?limit=10&difficulty=easy&tags=marvel')
+    .then(function(response){
         response.json().then(function(data){
           resolve(data)
         });
@@ -50,192 +38,16 @@ async function getMarvel() {
     });
 }
 
+// Giphy API fetch
 async function getGiphy() {
-  let giphyUrl = 'https://api.giphy.com/v1/gifs/G0bL1VhIULA0XwlHEwdsZFsRp2kbEO4k?api_key=' + giphyKey;
-  let giphyPng = await fetch(giphyUrl)
-    .then(function(response){
-    response.json().then(function(data){
-      resolve(data)
+  let keyword = questionsArray[currentQuestion].correctAnswer;
+  let giphyUrl = 'https://api.giphy.com/v1/gifs/search?api_key=' + giphyKey + "&q=" + keyword + "&limit=1&offset=0&rating=g&lang=en";
+  return await fetch(giphyUrl)
+    .then(response => response.json())
+    .then(json => {
+      return json.data[0].images.fixed_height.url;
     });
-  return giphyPng;
-  // }).catch(function(error){
-  //   reject(error);
-  });
-};
-
-// to fetch trivia API
-function generateQuestions() {
-  viewGamePage();
-  if (currentQuestion > 9) {
-    score = correctAnswersCount;
-    clearInterval(timer);
-    gameOver();
-    viewFormPage();
-    return;
-  }
-
-  let questionText = document.querySelector(".questionText");
-  let answerList = document.querySelector(".answerList");
-  let answerSelected = document.querySelector(".answerSelected");
-  console.log(answerSelected);
-  answerSelected.style.display = "none";
-  answerList.style.display = "block"; 
-
-  let choices = questionsArray[currentQuestion].incorrectAnswers;
-  choices.push(questionsArray[currentQuestion].correctAnswer);
-  
-  let randomChoices = _.sample(choices, 4);
-
-  questionText.innerHTML = '<div class="questionText"><span>' + (currentQuestion+1) + '. ' + questionsArray[currentQuestion].question + '</span>'; 
-  //shows answers choices in quiz box in order it was written in questions object array
-  answerList.innerHTML = '<div class="option"><span>'+ randomChoices[0] +'</span></div>'
-  + '<div class="option"><span>'+ randomChoices[1] +'</span></div>'
-  + '<div class="option"><span>'+ randomChoices[2] +'</span></div>'
-  + '<div class="option"><span>'+ randomChoices[3] +'</span></div>'; 
-
-  //grabs all choices array from questions array object
-  let options = answerList.querySelectorAll(".option");
-
-  //adds even listener for when player clicks on their choice
-  for (i=0; i < options.length; i++){
-    options[i].setAttribute("onclick", "optionSelected(this)");
-  }
 }
-
-function optionSelected(selection) {
-    if (selection.innerText === questionsArray[currentQuestion].correctAnswer) {
-        selection.style.backgroundColor = 'green';
-    } else {
-        selection.style.backgroundColor = 'red';
-    }
-
-  document.querySelector(".answerList").style.display = "none";;
-  document.querySelector(".answerSelected").style.display = "block";;
-
-  let timer = setInterval(function() {
-    generateQuestions();
-    clearInterval(timer);
-  }, 5000);
-}
-
-// start game funtion
-startgameBtn.onclick = async function() {
-  document.querySelector(".timeText").innerHTML = "Time: ";
-  document.getElementById("secsLeft").style.display = "block";
-
-  timeLeft = 15;
-  currentQuestion = 0;
-  //timer();
-  await getMarvel(questionsArray).then(result => {
-    questionsArray = result;
-    console.log(questionsArray);
-  });
-  generateQuestions(currentQuestion);
-}
-// getGiphy().then(function(data){
-//   for(let i = 0; i < data.results.length; i++){
-//     if(data.results[i].question.includes("Marvel")){
-//       let question = document.createElement("p");
-//       question.innerHTML = data.results[i].question;
-//       questionEl.appendChild(question);
-//       break;
-//     }
-//   }
-//   console.log(data);
-// });
-
-
-// Set clock, remove button, and start 60 seconds, begin game
-// function gameInit() {
-//     highScore = JSON.parse(localStorage.getItem("xxx"));
-//     correctInput = 0;
-//     incorrectInput = 0;
-//     quizIndex = 0;
-//     timeLeft = 60;
-//     startButton.css("display", "none");
-//     displayQuestion(quizObject[quizIndex]);
-//     timer();
-// }
-
-
-// Function to display questions and correct answers
-// function displayQuestion(myObject) {
-
-//     questEl.text(myObject.question);
-
-//     let order = Math.round(Math.random() * 3);
-//     for (i=0; i<3; i++) {
-//         let answerTrue = $("xxx").addClass("xxx");
-//         let answerWrong = $("xxx").addClass("xxx");
-
-//         if (order === i && order < 3) {
-//             answerTrue.text(myObject.true);
-//             answerOrdered.append(answerTrue);
-//             answerWrong.text(myObject.wrong[i]);
-//             answerOrdered.append(answerWrong);
-//         }
-//         else if (order === 3 && i === 2) {
-
-//             answerWrong.text(myObject.wrong[i]);
-//             answerOrdered.append(answerWrong);
-
-//             answerTrue.text(myObject.true);
-//             answerOrdered.append(answerTrue);
-//         }
-//         else {
-
-//             answerWrong.text(myObject.wrong[i]);
-//             answerOrdered.append(answerWrong);
-//         }
-//     }
-// }
-
-
-// Create the timer function and to have run smoothly
-// function timer() {
-//     var timeInterval = setInterval(function () {
-
-//         if (timeLeft > 0 && quizIndex !== quizObject.length){
-//             game.text(`You have ${timeLeft} seconds left.`);
-//             timeLeft--;
-//         }
-
-//         // Completed the questions or time has run out
-//         else (timeLeft === 0 || quizIndex === quizObject.length);
-
-//     },1000);
-// };
-
-// Write an event listener for start button
-// Start button will begin game
-// startButton.on("click", function(event) {
-//     let element = event.target;
-//     if (element.matches("button") === true) {
-//         // Initialize Game
-//         gameInit();
-//     }
-//     console.log(event);
-// })
-
-// Write an event listener for answer selection
-// answerOrdered.on("click", function(event) {
-//     let element = event.target;
-//     if (element.matches("li") === true) {
-
-//         // Move to the following question
-//         if (quizIndex < quizObject.length) {
-
-//             questEl.text('');
-
-//             answerOrdered.html('');
-//             displayQuestion(quizObject[quizIndex]);
-//         };
-//     };
-// });
-// Monineath's section >>>>>
-
-// start game page elements
-// var startGameSection = 
 
 // to display highscore page
 function viewHighScore() {
@@ -265,8 +77,107 @@ function viewGamePage() {
   highscoreSection.style.display = "none";
   hsFormSection.style.display = "none";
 }
-// <<<<< Monineath's section
 
+// using the fetched trivia API to generate the questions
+function generateQuestions() {
+  questionTimer();
+  viewGamePage();
+  if (currentQuestion > 9) {
+    score = correctAnswersCount;
+    clearInterval(timer);
+    gameOver();
+    viewFormPage();
+    return;
+  }
+
+  let questionText = document.querySelector(".questionText");
+  let answerList = document.querySelector(".answerList");
+  let answerSelected = document.querySelector(".answerSelected");
+
+  answerSelected.style.display = "none";
+  answerList.style.display = "block"; 
+
+  let choices = questionsArray[currentQuestion].incorrectAnswers;
+  choices.push(questionsArray[currentQuestion].correctAnswer);
+  
+  let randomChoices = _.sample(choices, 4);
+
+  questionText.innerHTML = '<div class="questionText"><span>' + (currentQuestion+1) + '. ' + questionsArray[currentQuestion].question + '</span>'; 
+  //shows answers choices in quiz box in order it was written in questions object array
+  answerList.innerHTML = '<div class="option"><span>'+ randomChoices[0] +'</span></div>'
+  + '<div class="option"><span>'+ randomChoices[1] +'</span></div>'
+  + '<div class="option"><span>'+ randomChoices[2] +'</span></div>'
+  + '<div class="option"><span>'+ randomChoices[3] +'</span></div>'; 
+
+  //grabs all choices array from questions array object
+  let options = answerList.querySelectorAll(".option");
+
+  //adds even listener for when player clicks on their choice
+  for (i=0; i < options.length; i++){
+    options[i].setAttribute("onclick", "optionSelected(this)");
+  }
+}
+
+// function for when player selects on an answer option
+async function optionSelected(selection) {
+    if (selection.innerText === questionsArray[currentQuestion].correctAnswer) {
+        selection.style.backgroundColor = 'green';
+        document.querySelectorAll(".option").forEach(function(option) {
+          if (option.innerText !== questionsArray[currentQuestion].correctAnswer) {
+            option.style.display = "none";
+          }
+        });
+        await getGiphy().then(result => {
+
+          document.querySelector(".answerSelected").style.display = "flex";
+          document.querySelector(".answerSelected").style.justifyContent = "center";
+          document.getElementById("answerImg").src = result;
+        });
+    } else {
+        selection.style.backgroundColor = 'red';
+    }
+  currentQuestion++;
+
+  let timer = setInterval(function() {
+    generateQuestions();
+    clearInterval(timer);
+  }, 3000);
+}
+
+// start game button on main page
+startgameBtn.onclick = async function() {
+  document.querySelector(".timeText").innerHTML = "Time: ";
+  document.getElementById("secsLeft").style.display = "block";
+
+  timeLeft = 15;
+  currentQuestion = 0;
+  //timer();
+  await getMarvel(questionsArray).then(result => {
+    questionsArray = result;
+  });
+  generateQuestions(currentQuestion);
+}
+
+
+// Create the timer function and to have run smoothly
+// function timer() {
+//     var timeInterval = setInterval(function () {
+
+//         if (timeLeft > 0 && quizIndex !== quizObject.length){
+//             game.text(`You have ${timeLeft} seconds left.`);
+//             timeLeft--;
+//         }
+
+//         // Completed the questions or time has run out
+//         else (timeLeft === 0 || quizIndex === quizObject.length);
+
+//     },1000);
+// };
+
+// <<<<< Monineath's section
+function questionTimer() {
+  
+}
 
 // Uday's gameover section
 function play(quiz) {
